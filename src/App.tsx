542bd44d3d3397d8080b0b94d795d9ad875d8d7e
jlaguilargomez <jlaguigom@gmail.com>
2022-04-30 17:04:28 +0200

    Dealing with base styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import './App.scss';
 
 function App(): JSX.Element {
     return (
-        <div className="App">
+        <div className="container">
             <h1>SOmething</h1>
         </div>
     );

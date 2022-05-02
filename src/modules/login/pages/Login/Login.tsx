5a8a3169c16c570aae36aa254ed8a0509280659c
jlaguilargomez <jlaguigom@gmail.com>
2022-05-02 11:10:36 +0200

    Add styles of Login Page

--- src/modules/login/pages/Login/Login.tsx
+++ src/modules/login/pages/Login/Login.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import cn from 'classnames';
 
 import LoginForm from 'modules/login/containers/LoginForm/LoginForm';
+
+import styles from 'styles/pages/LoginPage.module.scss';
 
 function Login(): JSX.Element {
     return (
         <>
-            <h1>Hello</h1>
+            <h1 className={cn(styles['login-page'], 'title')}>¡Bienvenid@!</h1>
             <LoginForm />
         </>
     );

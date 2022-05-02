564f6aaffeb86f5025a248c1b7d0feaf19befbac
jlaguilargomez <jlaguigom@gmail.com>
2022-05-02 10:25:30 +0200

    Create base routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import NotFound from 'modules/common/containers/NotFound/NotFound';
+import Landing from 'modules/dashboard/containers/Landing/Landing';
+import Login from 'modules/login/containers/Login/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App(): JSX.Element {
     return (
-        <div className="container">
-            <h1>SOmething</h1>
-        </div>
+        <BrowserRouter>
+            <div className="container">
+                <Routes>
+                    <Route path="/" element={<Landing />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
---
+++ src/modules/dashboard/containers/Landing/Landing.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Landing(): JSX.Element {
+    return <div>Landing</div>;
+}
+
+export default Landing;

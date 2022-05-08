960b9174fe08f1f4d2939f6a7afe361e900afa22
jlaguilargomez <jlaguigom@gmail.com>
2022-05-08 12:31:29 +0200

    Add contact info

--- src/modules/info/containers/Contact/Contact.tsx
+++ src/modules/info/containers/Contact/Contact.tsx
@@ -1,17 +1,50 @@
 import React from 'react';
 
+import WhatsappIcon from 'assets/svg/WhatsApp.svg';
+import PhoneIcon from 'assets/svg/phone-call.svg';
+
+import styles from 'styles/containers/Contact.module.scss';
+
+interface ContactInfoProps {
+    name: string;
+    phoneNumber: string;
+}
+
+function ContactInfo({ name, phoneNumber }: ContactInfoProps): JSX.Element {
+    return (
+        <div className={styles['contact-info']}>
+            <div className={styles['contact-info__row']}>
+                <p className={styles['contact-info__column']}>
+                    <span>{name}</span>
+                    <span>{phoneNumber}</span>
+                </p>
+
+                <a href={`https://wa.me/+34${phoneNumber}`}>
+                    <img
+                        className={styles.contact__icon}
+                        src={WhatsappIcon}
+                        alt="whatsapp icon"
+                    />
+                </a>
+                <a href={`tell:+34${phoneNumber}`}>
+                    <img
+                        className={styles.contact__icon}
+                        src={PhoneIcon}
+                        alt="whatsapp icon"
+                    />
+                </a>
+            </div>
+        </div>
+    );
+}
+
 function Contact(): JSX.Element {
     return (
         <>
             <h2 className="secondary-title">Contacto</h2>
             <div>
-                <p>
-                    Jose Luis{' '}
-                    <a href="https://wa.me/+34616379123">616 37 91 23</a>
-                </p>
-                <p>
-                    Maru <span>620 75 40 67</span>
-                </p>
+                <ContactInfo name="Jose Luis" phoneNumber="616379123" />
+                <ContactInfo name="Maru" phoneNumber="620754067" />
             </div>
         </>
     );
